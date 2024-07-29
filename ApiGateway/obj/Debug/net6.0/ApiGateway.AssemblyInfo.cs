e00fc30b859eda291d7d0e3e6781cb66a414080e
2E4O8T <benoit.froidevan@axa.fr>
2024-07-29 18:00:15 +0200

    Third-commit

--- ApiGateway/obj/Debug/net6.0/ApiGateway.AssemblyInfo.cs
+++ ApiGateway/obj/Debug/net6.0/ApiGateway.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+940504e39b4386eaeecb4988249ba15307bd7191")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8006f6507bf8e275f79067e036a851daecf7706")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
